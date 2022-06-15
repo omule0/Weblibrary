b224fe007538e7bfb13d95942b62f1234d429002
NDYAMANYI MARVIN SATULO <marvovybes23@gmail.com>
2022-06-15 22:24:38 +0300

    Added templates folder per app to store the app's html files ,  added comments and methods to use when writing the first prototype

--- VirtualEnvironment/weblibrary/account/views.py
+++ VirtualEnvironment/weblibrary/account/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#views for the home app
+    #view to Create a New Account
+    #view to login users
+    #view to logout users
+
+def createNewAccount(request):
+    #create student account | librarian account
+    pass
+
+def login(request):
+    #login to student account | librarian account
+    pass
+
+def logout(request):
+    #logout from any account currently logged in
+    pass
