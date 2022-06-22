527e0d418d8ca1aa0a38b0089fd97d5dde3a56ff
jamespapa <bornjimmy213@gmail.com>
2022-06-22 09:01:22 +0300

    i have added apps

--- VirtualEnvironment/weblibrary/weblibrary/urls.py
+++ VirtualEnvironment/weblibrary/weblibrary/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from home import views
+
+
+#def james(request):
+ #   return HttpResponse('hello world')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , views.james)
 ]
