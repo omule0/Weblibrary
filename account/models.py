287f7174681991c1e39b9693097312f4dae0fd14
NDYAMANYI MARVIN SATULO <marvovybes23@gmail.com>
2022-06-23 23:44:25 +0300

    Added model StudentReg in account app models.py to create a table in the database for storing the data captued from the student during account creation

--- account/models.py
+++ account/models.py
@@ -1,5 +1,13 @@
+import email
 from multiprocessing import AuthenticationError
 from turtle import title
 from django.db import models
 
 #model to add book
+
+#model to capture student details during registration
+class StudentReg(models.Model):
+    firstname =models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    mail = models.EmailField(default="library@email.tld")
+    password = models.CharField()
