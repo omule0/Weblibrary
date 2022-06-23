1d99fe553dc1d7c873bc175de081720d2d1bf09e
NDYAMANYI MARVIN SATULO <marvovybes23@gmail.com>
2022-06-24 00:23:19 +0300

    fixed all errors with the database and onjects can now be created

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path 
 from . import views
 
-urlpatterns = [path('studentreg/', views.StudentReg)]
+urlpatterns = [path('studentreg/', views.Student_Reg)]
--- account/views.py
+++ account/views.py
@@ -1,14 +1,14 @@
-from account import models
+from . import models
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def StudentReg(request):
+def Student_Reg(request):
     if request.method == "POST":
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']
-        save_student_details = StudentReg(firstname=first_name , lastname=last_name , mail=email , password=password)
+        save_student_details = models.StudentReg(FirstName=fname ,LastName=lname ,mail=email, password=password)
         save_student_details.save()
     return render(request , 'student_reg.html' )
 
