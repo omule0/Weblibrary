0f5780b12bc149355de7c93d5fe6a2adf178617f
NDYAMANYI MARVIN SATULO <marvovybes23@gmail.com>
2022-06-28 22:28:20 +0300

    Added a simple homepage to replace the old one

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path 
 from . import views
 
-urlpatterns = [path('studentreg/', views.Student_Reg)]
+urlpatterns = [path('studentreg/', views.Student_Reg), 
+path('studentlogin/' ,views.Student_Login)]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from . import models
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
 
 def Student_Reg(request):
     if request.method == "POST":
@@ -11,6 +13,24 @@ def Student_Reg(request):
         save_student_details = models.StudentReg(FirstName=fname ,LastName=lname ,mail=email, password=password)
         save_student_details.save()
     return render(request , 'student_reg.html' )
+
+
+def Student_Login(request):
+    if request.method == "GET":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user != None:
+            auth.login(request , user)
+            return redirect("/")
+        else:
+            messages.info(request , 'invalid credentials')
+            return redirect("student_login")
+
+    
+        
+
+    return render(request , 'Student_Login.html')
 
 
 
