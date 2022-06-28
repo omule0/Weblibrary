13514905337eed4e07630a29965b4d2096d763af
NDYAMANYI MARVIN SATULO <marvovybes23@gmail.com>
2022-06-28 23:54:45 +0300

    Updated modes.py in the accounts app with fields matching the ones implemented in the studentSignup.html form

--- account/models.py
+++ account/models.py
@@ -3,9 +3,11 @@ from turtle import title
 from django.db import models
 
 #model to capture student details during registration
+class studentSignup(models.Model):
+    FIRSTNAME =models.CharField(max_length=255)
+    LASTNAME = models.CharField(max_length=255)
+    EMAIL = models.EmailField(default="citizen0@utopia.com")
+    PASSWORD = models.CharField(max_length=255)
+    DATE_OF_BIRTH = models.DateField()
+    GENDER = models.BooleanField(default=None)
 
-class StudentReg(models.Model):
-    FirstName =models.CharField(max_length=255)
-    LastName = models.CharField(max_length=255)
-    mail = models.EmailField(default="library@email.tld")
-    password = models.CharField(max_length=255)
