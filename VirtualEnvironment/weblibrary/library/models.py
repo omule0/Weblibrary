3584e20e4128efc9fa9e1e5c1bf0e71f1b53753f
jamespapa <bornjimmy213@gmail.com>
2022-07-20 22:20:59 +0300

    made some changes in the models

--- VirtualEnvironment/weblibrary/library/models.py
+++ VirtualEnvironment/weblibrary/library/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField('Categories', max_length=200)
+    name = models.CharField('Categories', max_length=100)
     slug = models.SlugField(max_length=200)
 
     def __str__(self):
