7d076f1f94b111cb1f5d3a22ddb9affcedce8bfe
jamespapa <bornjimmy213@gmail.com>
2022-07-20 22:25:43 +0300

    modified models

--- VirtualEnvironment/weblibrary/library/models.py
+++ VirtualEnvironment/weblibrary/library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField('Categories', max_length=100)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=100)
 
     def __str__(self):
         return self.name
