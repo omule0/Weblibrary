92d0d6dd6449a3e7f766401eae6e4460053a1cc1
jamespapa <bornjimmy213@gmail.com>
2022-07-20 22:49:49 +0300

    also modified on the maximum lengthsof the email and password 2 they r too long

--- VirtualEnvironment/weblibrary/account/forms.py
+++ VirtualEnvironment/weblibrary/account/forms.py
@@ -8,14 +8,14 @@ class CreateUserForm(UserCreationForm):
         'class': 'form-control', 'placeholder': 'Please Enter your full name'
     }))
 
-    email = forms.CharField(max_length=100, widget=forms.EmailInput(attrs={
+    email = forms.CharField(max_length=50, widget=forms.EmailInput(attrs={
         'class': 'form-control', 'placeholder': 'Enter Email Address'
     }))
 
     password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
         'class': 'form-control', 'placeholder': 'At least eight characters'
     }))
-    password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
         'class': 'form-control', 'placeholder': 'Please Confirm Password'
     }))
 
