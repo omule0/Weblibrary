be3159416e5678da1679189dd6168bcf7afda04c
Hisaalu <hisaalujuniornelson@gmail.com>
2022-07-25 19:36:07 +0300

    created a path for the home app in urls.py file

--- weblibrary/urls.py
+++ weblibrary/urls.py
@@ -18,6 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# Django admin header customizations
+admin.site.site_header = "LIBRARY CONTROL PANEL"
+admin.site.site_title = "LOG IN AS LIBRARIAN"
+# admin.site.index_title = ""
+
 urlpatterns = [
                   path('', include('home.urls')),
                   path('admin/', admin.site.urls),
